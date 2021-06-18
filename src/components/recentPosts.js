d1c5b1cd76d79e133a4104f26f97991e8d867182
Teddy Nadaskay <tnadaskay@icloud.com>
2021-06-18 12:58:16 -0600

    created recentposts and searchbar components

---
+++ src/components/recentPosts.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className='Recent-Posts'>
+                <div ClassName='Recent-Posts-Wrapper'>
+                    <div ClassName='Recent-Posts-Heading'Recent Posts>
+                        <ul ClassName='Recent-Posts__Posts'> 
+                            <li>
+                                Recent Post 1
+                            </li>
+                            <li>
+                                Recent Post 2
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBar; 
